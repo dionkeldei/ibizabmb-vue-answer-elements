6beff2286a0f7415d2a158596facc4a4959bc977
Dionkeldei <dionkeldei@gmail.com>
2021-04-08 17:19:40 -0500

    checkbox

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="ibizabmb-answer-elements">
      <selection v-if="element.type == 'select'" :element="element" @change="change"/>
-     <radio v-if="element.type == 'radio'" :element="element" :index="element.name" @change="change"/>
-     <radio-comment v-if="element.type == 'radio-comment'" :element="element" :index="element.name" @change="change"/>
+     <radio v-if="element.type == 'radio'" :element="element" @change="change"/>
+     <radio-comment v-if="element.type == 'radio-comment'" :element="element" @change="change"/>
+     <checkboxes v-if="element.type == 'checkbox'" :element="element" @change="change"/>
   </div>
 </template>
 
@@ -10,13 +11,14 @@
 import selection from '@/components/select.vue'
 import radio from '@/components/radio.vue'
 import radioComment from '@/components/radio_comment.vue'
+import checkboxes from '@/components/checkbox.vue'
 export default {
   name: 'ibiza-answer-elements',
   data () {
     return {
       element: {
         index: 56,
-        type: 'radio-comment',
+        type: 'checkbox',
         fields: [
           {
             text: 'campo1',
@@ -33,7 +35,8 @@ export default {
   components: {
     selection,
     radio,
-    radioComment
+    radioComment,
+    checkboxes
   },
   methods: {
     change: function (val, index) {
---
+++ src/components/checkbox.vue
@@ -0,0 +1,50 @@
+<template lang="html">
+  <div class="">
+    <div v-for="(field,i) in element.fields" :key="i">
+      <label>
+        <input :value="field.value" class="uk-checkbox" :name="'ibiza_checkbox_answer_'+element.index" type="checkbox" v-model="values[i]"/> {{field.text}}
+      </label>
+      <br>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ibizaCheckboxElement',
+  data () {
+    return {
+      values: []
+    }
+  },
+  props: {
+    element: Object
+  },
+  methods: {
+  },
+  computed: {
+    getValue: function () {
+        if(this.element.value != undefined){
+          return this.element.value
+        }
+        var arr = []
+        for(var i=0;i<this.element.fields.length;i++){
+          arr.push(false)
+        }
+        console.log(arr)
+        return arr
+    }
+  },
+  mounted() {
+    this.values = this.getValue
+  },
+  watch: {
+    values: function (val) {
+      this.$emit('change',val,this.element.index)
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
